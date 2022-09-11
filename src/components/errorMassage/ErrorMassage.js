3a515497d624532677123d0069d0734daeb7eab5
Yevhenii <norman78795@gmail.com>
2022-09-11 16:36:59 +0300

    add error and spinner

---
+++ src/components/errorMassage/ErrorMassage.js
@@ -0,0 +1,10 @@
+/* eslint-disable jsx-a11y/alt-text */
+import img from './error.gif'
+
+const ErrorMassage = () => {
+    return (
+        <img style={{display: 'block', width: '250px', height: '250px', objectFit: 'contain', margin:'0 auto'}} src={img} alt="Error" />
+    )
+}
+
+export default ErrorMassage;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMassage from '../errorMassage/ErrorMassage';
 import MarvelService from '../../services/MarvelService';
-
+ 
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
@@ -10,43 +12,44 @@ class RandomChar extends Component {
         this.updateChar();
     }
     state = {
-        char: {}
+        char: {},
+        loading: true,
+        error: false
     }
     marvelService = new MarvelService();
 
     onCharLoaded = (char) => {
-        this.setState({char})
+        this.setState({
+            char,
+            loading: false})
     }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    } 
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
+            .catch(this.onError)
     }
 
     render() {
-        const {char: {name, description, thumbnail, homepage, wiki}} = this.state;
-         
+        const {char, loading, error} = this.state;
+        const errorMassage = error ? <ErrorMassage/> : null;
+        const spinner = loading ? <Spinner/>: null;
+        const content = !(loading || error) ? <View char = {char}/> : null;
+
         return (
         <div className="randomchar">
-            <div className="randomchar__block">
-                <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                <div className="randomchar__info">
-                    <p className="randomchar__name">{name}</p>
-                    <p className="randomchar__descr">
-                        {description}
-                    </p>
-                    <div className="randomchar__btns">
-                        <a href={homepage} className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href={wiki} className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
-                    </div>
-                </div>
-            </div>
+            {errorMassage} 
+            {spinner}
+            {content}
             <div className="randomchar__static">
                 <p className="randomchar__title">
                     Random character for today!<br/>
@@ -64,5 +67,30 @@ class RandomChar extends Component {
         )
     }
 }
+
+const View = ({char}) => {
+    const {name, description, thumbnail, homepage, wiki} = char;
+
+    return (
+        <div className="randomchar__block">
+        <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">
+                    {description}
+                </p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    ) 
+} 
 
 export default RandomChar;
